79a5387b1f57e06536c96135001c762608242f8d
Mostafa <mostafa.ghadimi@yahoo.com>
2019-08-22 17:04:28 +0430

    process added

---
+++ process.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body{ 
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+            direction: rtl;
+        }
+    </style>
+</head>
+<body>
+    <div style="width: 70%; background-color: rgb(0, 94, 8); color: white; padding: 10px; margin: 30px auto; font-family: 'B Yekan'">
+        پرداخت شما با موفقیت انجام شد
+    </div>
+</body>
+</html>
